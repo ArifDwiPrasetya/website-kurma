73c5df858971de24e677d5bc98920738b7165354
ArifDwiPrasetya <arifdwiprasetya14@gmail.com>
2026-01-30 09:33:20 +0800

    update Image astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import icon from "astro-icon";
+import icon from "astro-icon"; // 1. Import library icon
 
 // https://astro.build/config
 export default defineConfig({
-  // Pastikan output diset ke static jika Anda ingin deploy di EdgeOne Pages biasa
-  output: "static",
-
-  integrations: [icon()],
-
+  integrations: [
+    icon(), // 2. Tambahkan ini di array integrations
+  ],
   vite: {
     plugins: [tailwindcss()],
-    // Tambahkan ssr noexternal untuk mengatasi masalah inisialisasi modul
-    ssr: {
-      noExternal: ["astro"],
-    },
-  },
-
-  image: {
-    // Gunakan 'squoosh' sebagai alternatif jika 'sharp' bermasalah di lingkungan build
-    service: {
-      entrypoint: "astro/assets/services/squoosh",
-    },
   },
 });
