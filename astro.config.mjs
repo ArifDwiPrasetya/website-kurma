76d413abce64567550b0e15ac144b1e00f8905da
ArifDwiPrasetya <arifdwiprasetya14@gmail.com>
2026-02-02 17:57:49 +0800

    add astro-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon"; // 1. Import library icon
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://website-kurma.edgeone.dev",
-  integrations: [
-    icon(), // 2. Tambahkan ini di array integrations
-  ],
+  integrations: [// 2. Tambahkan ini di array integrations
+  icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
